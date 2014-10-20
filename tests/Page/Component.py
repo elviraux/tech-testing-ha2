cc5d3d7c1e4ea27f9634932f1ca15a779d05468a
kolesofortuni <kolesofortuni@mail.ru>
2014-10-21 01:10:28 +0400

    banner form done

--- tests/Page/Component.py
+++ tests/Page/Component.py
@@ -1,14 +1,21 @@
 __author__ = 'Elvira'
 
 from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 
 class Component(object):
     def __init__(self, driver):
         self.driver = driver
 
-    def wait(self, selector):
-        return WebDriverWait(self.driver, 30, 0.1).until(
+    def wait(self, DRIVER, selector):
+        return WebDriverWait(DRIVER, 30, 0.1).until(
             lambda d: d.find_element_by_css_selector(selector)
+        )
+
+    def wait_id(self, selector):
+        return WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_id(selector)
         )
 
 
@@ -37,12 +44,111 @@ class TopMenu(Component):
 
     @property
     def get_email(self):
-        return self.wait(self.EMAIL)
+        return self.wait(self.driver, self.EMAIL)
 
 class BannerElement(Component):
 
-    def get_form(self):
-        return self.driver.find_element_by_class_name('banner-form')
+    TITLE = '.banner-form__input[data_name=title]'
+    DESCRIPTION = '.banner-form__input_text-area[data-name=text]'
+    TARGET = 'input.banner-form__input-no-utm'
+    IMAGE1 = '.banner-form__img-file'
+    IMAGE2 = '.banner-form__img-file[data-name=promo_image]'
+    SAVE = '.banner-form__save-button'
+
+    @staticmethod
+    def waiting(driver):
+        IMG = '.banner-preview__img'
+        banners = driver.find_elements_by_css_selector(IMG)
+        for banner in banners:
+            if banner.value_of_css_property("display") == 'block':
+                return banner
+
+    def banner(self):
+        return WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('.banner-form')
+        )
+
+    def set_title(self, header):
+
+        banner_title = WebDriverWait(self.banner(), 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('input[data-name=title]')
+        )
+        banner_title.send_keys('TITLE OLOLOLO')
+
+    def set_description(self, description):
+        element = self.wait(self.banner(), self.DESCRIPTION)
+        element.send_keys(description)
+
+    def set_target(self, url):
+        elements = WebDriverWait(self.banner(), 30, 0.1).until(
+            lambda d: d.find_elements_by_css_selector(self.TARGET)
+        )
+        target = None
+        for e in elements:
+            if e.is_displayed():
+                target = e
+                break
+        if target is None:
+            raise Exception("Couldn't find element")
+
+        target.send_keys(url)
+
+    def upload_min_img(self, img):
+        BANNER_PREVIEW ='.banner-preview__middleleft'
+        CROPPER = '.image-cropper__save'
+
+        element = self.wait(self.banner(), self.IMAGE1)
+        element.send_keys(img)
+        self.wait(self.driver, CROPPER).click()
+        img1 = self.banner().find_element_by_css_selector(BANNER_PREVIEW)
+        WebDriverWait(img1, 30, 0.1).until(
+            self.waiting
+        )
+
+    def upload_max_img(self, img):
+        BANNER_PREVIEW = '.banner-preview__bottom'
+        CROPPER = '.image-cropper__save'
+
+        element = self.wait(self.banner(), self.IMAGE2)
+        element.send_keys(img)
+        self.wait(self.driver, CROPPER).click()
+
+        img2 = self.banner().find_element_by_css_selector(BANNER_PREVIEW)
+        WebDriverWait(img2, 30, 0.1).until(
+            self.waiting
+        )
+
+
+
+    def save_banner(self):
+        elements = WebDriverWait(self.driver, 30, 0.1).until(
+            lambda p: p.find_elements_by_css_selector(self.SAVE)
+        )
+        button = None
+        for e in elements:
+            if e.is_displayed():
+                button = e
+                break
+        if button is None:
+            raise Exception("Couldn't find element")
+        button.click()
+
+
+    def create_banner(self):
+        HEADER = "Header"
+        DESCRIPTION = "Description"
+        TARGET = "http://www.odnoklassniki.ru/target"
+        IMG1 = 'tests/source/1.jpg'
+        IMG2 = 'tests/source/2.jpg'
+
+        self.set_title(HEADER)
+        self.set_description(DESCRIPTION)
+        self.set_target(TARGET)
+        self.upload_min_img(IMG1)
+        self.upload_max_img(IMG2)
+        self.save_banner()
+
+
 
 
 class BaseSettingsElement(Component):
@@ -52,7 +158,7 @@ class BaseSettingsElement(Component):
     PLATFORM = "pad-mobile_odkl_feed_abstract"
 
     def create(self):
-        self.wait(self.CREATE).click()
+        self.wait(self.driver, self.CREATE).click()
 
     def set_name(self, company_name):
         element = self.driver.find_element_by_css_selector(self.COMPANY_NAME)
@@ -66,57 +172,45 @@ class BaseSettingsElement(Component):
         self.driver.find_element_by_id(self.PLATFORM).click()
 
 
-class BannerElement(Component):
-
-    TITLE = '.banner-form__input[data_name=title]'
-    DESCRIPTION = '.banner-form__input_text-area[data-name=text]'
-    TARGET = '.banner-form__input[data-name=url]'
-
-    IMAGE1 = '.banner-form__img-file'
-    IMAGE2 = '.banner-form__img-file[data-name=promo_image]'
-    AGE = "restrict-0+"
-    PLACE_LIST = 'campaign-setting__preset-list'
-    PLACE = '[data-name=russia]'
-
-    #def get_banner(self):
-        #return BannerElement(self.driver).get_form()
-
-    def set_title(self, header):
-        element = wait(self.TITLE)
-        element.send_keys(header)
-
-    def set_description(self, description):
-        element = wait(self.DESCRIPTION)
-        element.send_keys(description)
-
-    def set_target(self, target):
-        element = wait(self.TARGET)
-        element.send_keys(target)
-
-    def upload_min_img(self, img):
-        element = wait(self.IMAGE1)
-        element.send_keys(img)
-
-    def upload_max_img(self, img):
-        element = wait(self.IMAGE2)
-        element.send_keys(img)
-
 
 class WhomElement(Component):
-    RESTRICT = 'campaign-setting__wrapper_restrict'
+    RESTRICT = '.campaign-setting__wrapper_restrict'
     SHOW_RESTRICT = '[data-node-id=restrict]'
     RESTRICT12 = 'restrict-12+'
 
     def choose_restrict(self):
-        restrict = wait(self.RESTRICT)
+        restrict = self.wait(self.RESTRICT)
         restrict.find_element_by_css_selector(self.SHOW_RESTRICT).click()
-        restrict_btn =  wait(self.RESTRICT12)
+        restrict_btn =  self.wait_id(self.RESTRICT12)
         restrict_btn.click()
 
 
 class WhereElement(Component):
+    PLACE_LIST = '.campaign-setting__preset-list'
+    PLACE = 'li.campaign-setting__preset[data-value=188]'
     def choose_place(self):
-        place_list  = WebDriverWait(self.driver, 30, 1).until(
-            lambda d: d.find_element_by_class_name(self.PLACE_LIST)
+
+
+        place_list = WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('.campaign-setting__preset-list')
         )
+        place_element = WebDriverWait(place_list, 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('[data-name=russia]')
+        )
+
+        place_wrapper  =  WebDriverWait(self.driver, 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('.campaign-setting__wrapper_regions')
+        )
+        place_rus = WebDriverWait(place_wrapper, 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('.tree__node[id=regions188]')
+        )
+        place_rus_btn = WebDriverWait(place_rus, 30, 0.1).until(
+            lambda d: d.find_element_by_css_selector('.tree__node__input')
+        )
+        if not place_rus_btn.is_selected():
+            place_rus_btn.click()
+            place_rus_btn.get_attribute('checked')
+
+
+
 
--- tests/TestMakeGroup.py
+++ tests/TestMakeGroup.py
@@ -5,7 +5,6 @@ from selenium.webdriver import  DesiredCapabilities, Remote
 import os
 #from selenium.webdriver.support.ui import Select, WebDriverWait
 from tests.Page.Page import CreateCompanyPage, CurrentPage, LoginPage
-from tests.Page.Component import WhereElement, WhomElement
 
 
 
@@ -43,7 +42,7 @@ class TestTarget(unittest.TestCase):
         email = top_email.get_email().text
         assert EMAIL in email
     """
-
+    """
     def test_create(self):
         COMPANY_NAME ="Sunny"
 
@@ -54,46 +53,27 @@ class TestTarget(unittest.TestCase):
         base_settings.choose_company_type()
         base_settings.choose_platform()
 
-        whom_settings = create_page.whom_form()
+        whom_settings = create_page.whom_form
         whom_settings.choose_restrict()
 
-
-   # def test_restrict(self):
-
-
-"""
-    #def test(self):
+        where_settings = create_page.where_form
+        where_settings.choose_place()
 
 
-        COMPANY_NAME ="Sunny"
-        HEADER = "Header"
-        DESCRIPTION = "Description"
-        TARGET = "http://www.odnoklassniki.ru/target"
-        IMG1 = 'tests/source/1.jpg'
-        IMG2 = 'tests/source/2.jpg'
+    """
+
+    def test(self):
 
 
-        current_page = CurrentPage(self.driver)
         create_page = CreateCompanyPage(self.driver)
-        email = create_page.top_menu.get_email()
-
-        create_form = create_page.create_form()
-        create_form.create()
-        create_form.set_name(COMPANY_NAME)
+        create_page.open()
+        create_form = create_page.base_form()
         create_form.choose_company_type()
         create_form.choose_platform()
+        create_form = create_page.banner_form()
+        create_form.create_banner()
 
 
-      #  create_form.set_title(HEADER)
-        #create_form.set_description(DESCRIPTION)
-       # create_form.set_target(TARGET)
-        #create_form.upload_min_img(IMG1)
-        #create_form.upload_max_img(IMG2)
-        create_form.choose_restrict()
-       # create_form.choose_place()
-        place_list  = WebDriverWait(self.driver, 30, 1).until(
-            lambda d: d.find_element_by_css_selector('[data_name=where]')
-        )
 
 
 
@@ -105,7 +85,7 @@ class TestTarget(unittest.TestCase):
     #def test_create(self):
         #create_page = CreateCompanyPage(self.driver)
 
-"""
+
 
 if __name__ == "__main__":
     unittest.main()
